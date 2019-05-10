3f6db526e0567dfa24c0eb07f01cef3e1ca290f1
ben <bholsinger@gmail.com>
2019-05-10 15:32:39 -0600

    outline - sudo code

---
+++ scripts/app.js
@@ -0,0 +1,76 @@
+//requirments
+
+/*
+This game must have the following :
+
+object with health and health updates that are visable while game is played
+minimum of 6 buttons
+3 buttons to affect status of object.
+this three buttons must affect the object status in different amounts
+3 additional elements allowing user to affect how much the 3 buttons change the object status
+
+*/
+
+/*
+Variables:
+
+let tree= {};
+
+
+
+
+*/
+
+
+
+
+
+
+/*
+
+build objects:
+object is a tree
+
+
+
+
+*/
+
+
+
+/*
+build tools user uses to affect tree
+
+
+
+*/
+
+/*
+functions for buttons
+
+
+*/
+
+/*
+functions for radio specific buttons
+
+*/
+
+
+
+/*
+draw function
+this has methods that are use when user performs
+actions that change the page layout such as different picture
+
+
+
+*/
+
+
+/*
+functions that need used at start of page
+
+*/
+
+//initialize/call functions here
